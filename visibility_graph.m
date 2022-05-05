96cd910776df907d5089389a6470590028c52c11
Viral Panchal <viralpanchal1999@gmail.com>
2022-05-04 20:04:32 -0400

    Plots the possible paths from start to goal point.

--- visibility_graph.m
+++ visibility_graph.m
@@ -9,12 +9,12 @@ plot(arena,'Facecolor','white');
 
 % Making Obstacles within the arena
 x1 = [1.5 3.5 3.5 1.5];
-y1 = [1 1 3 3];
+y1 = [2 2 4 4];
 x2 = [3 5 5 3];
 y2 = [6.5 6.5 8.5 8.5];
 x3 = [6 8 8 6];
 y3 = [3 3 5 5];
-obstacles = [1.5 1 3.5 1 3.5 3 1.5 3;
+obstacles = [1.5 2 3.5 2 3.5 4 1.5 4;
              3 6.5 5 6.5 5 8.5 3 8.5;
              6 3 8 3 8 5 6 5];
          
@@ -34,6 +34,35 @@ y_g = 0.5;
 hold on 
 plot(x_s,y_s,'*');
 plot(x_g,y_g,'d');
+
+% checking if the start-point and goal-point have direct euclidien path 
+for i = 1:num_obstacles
+    if collision_check_segment(x_s,y_s,x_g,y_g,obstacles) == 0
+        hold on 
+        plot ([x_s x_g], [y_s y_g], 'b','LineWidth',2);
+        dist = sqrt((x_g - x_s)^2 + (y_g - y_s)^2);
+    end
+    pause(.1)
+end
+
+path_index = 1; 
+% making edges from start-point to neigbouring vertices
+for i = 1:num_obstacles
+    for m = 1:2:7
+        if collision_check_segment(x_s,y_s,obstacles(i,m),obstacles(i,m+1),obstacles) == 0
+            child_l1{1,path_index} = [x_s y_s;obstacles(i,m) obstacles(i,m+1)];
+            path_index = path_index + 1;
+            hold on 
+            plot ([x_s obstacles(i,m)], [y_s obstacles(i,m+1)], 'b');
+        end
+        pause(.1)
+    end
+end
+
+for i = 1:size(child_l1,2)
+    temp_tree = child_l1{1,i};
+    
+end
 
 % Connecting all the vertices
 for i = 1:length(x1)
@@ -68,23 +97,14 @@ for i = 1:length(x2)
     end
 end
 
-% making edges from start-point to neigbouring vertices
-for i = 1:num_obstacles
-    for m = 1:2:7
-        if collision_check_segment(x_s,y_s,obstacles(i,m),obstacles(i,m+1),obstacles) == 0
-            hold on 
-            plot ([x_s obstacles(i,m)], [y_s obstacles(i,m+1)], 'b');
-        end
-    end
-end
-
-
+% making edges from goal-point to neigbouring vertices 
 for i = 1:num_obstacles
     for m = 1:2:7
         if collision_check_segment(x_g,y_g,obstacles(i,m),obstacles(i,m+1),obstacles) == 0
             hold on 
             plot ([x_g obstacles(i,m)], [y_g obstacles(i,m+1)], 'g');
         end
+        pause(.1)
     end
 end
     
